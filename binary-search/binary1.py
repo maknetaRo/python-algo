2400386004f3825a3a3c09049b0a297ed9fb40b5
maknetaRo <makneta@gmail.com>
2020-06-03 22:46:17 +0200

    first implementation of binary search algorithm

---
+++ binary-search/binary1.py
@@ -0,0 +1,26 @@
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+
+        if guess == item:
+            return mid
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+
+# item = int(input("Write the number: "))
+my_list = [1, 3, 5, 7, 9, 11, 13, 14, 15, 22]
+print(binary_search(my_list, 13))
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, -1))
+
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, 9))
